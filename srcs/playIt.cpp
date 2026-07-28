2074c770612878ecbad3e2fe3ca2d4372f5470cb
txavier <tchiadeestevaox@gmail.com>
2026-07-28 19:23:03 +0100

    Refactor main function to implement SFML window rendering loop

--- srcs/playIt.cpp
+++ srcs/playIt.cpp
@@ -1,6 +1,23 @@
 #include "../includes/playIt.hpp"
 
-int	main(void)
+int main()
 {
-	
+    sf::RenderWindow window(
+        sf::VideoMode({800, 600}),
+        "Test SFML"
+    );
+
+    while (window.isOpen())
+    {
+        while (auto event = window.pollEvent())
+        {
+            if (event && event->is<sf::Event::Closed>())
+                window.close();
+        }
+
+        window.clear();
+        window.display();
+    }
+
+    return 0;
 }
