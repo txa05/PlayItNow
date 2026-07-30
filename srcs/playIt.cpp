0573f2a07c6706b2a454e0671a2e68e31243d007
txavier <tchiadeestevaox@gmail.com>
2026-07-30 18:12:21 +0100

    Refactor playIt.cpp: Update window size, shape initialization, and improve formatting

--- srcs/playIt.cpp
+++ srcs/playIt.cpp
@@ -1,23 +1,23 @@
-#include "../includes/playIt.hpp"
+# include "../includes/playIt.hpp"
+
+#include <SFML/Graphics.hpp>
 
 int main()
 {
-    sf::RenderWindow window(
-        sf::VideoMode({800, 600}),
-        "Test SFML"
-    );
+    sf::RenderWindow window(sf::VideoMode({200, 200}), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
 
     while (window.isOpen())
     {
         while (auto event = window.pollEvent())
         {
-            if (event && event->is<sf::Event::Closed>())
+            if (event->is<sf::Event::Closed>())
                 window.close();
         }
 
         window.clear();
+        window.draw(shape); 
         window.display();
     }
-
-    return 0;
 }
