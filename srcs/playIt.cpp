730be33600ff1e60b432620bac1578741f78c183
txavier <tchiadeestevaox@gmail.com>
2026-07-31 16:00:59 +0100

    Refactor playIt.cpp: Extract sound loading logic into findSound function and update main function structure

--- srcs/playIt.cpp
+++ srcs/playIt.cpp
@@ -1,21 +1,41 @@
 # include "../includes/playIt.hpp"
 
-int main()
+void	findSound(sf::Music &music, const std::string &path)
 {
-    sf::RenderWindow window(sf::VideoMode({200, 200}), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
 
-    while (window.isOpen())
-    {
-        while (auto event = window.pollEvent())
-        {
-            if (event->is<sf::Event::Closed>())
-                window.close();
-        }
+	if (!music.openFromFile(path))
+	{
+		std::cerr << "Failed to open sound file: " << path << std::endl;
+		return ;
+	}
+}
 
-        window.clear();
-        window.draw(shape); 
-        window.display();
-    }
+int	main(void)
+{
+
+	sf::Window	window(sf::VideoMode({800, 600}), "Testing");
+	if (!window.isOpen())
+		return (1);
+
+	sf::Music	m;
+	findSound(m, "songs/test.ogg");
+
+	//m.play();
+	while (window.isOpen())
+	{
+		while (auto event = window.pollEvent())
+		{
+			if (event->is<sf::Event::Closed>())
+				window.close();
+			if (const auto keyboard = event->getIf<sf::Event::KeyPressed>())
+			{
+				if (keyboard->scancode == sf::Keyboard::Scan::Escape)
+				{
+					window.close();
+				}
+			}
+		}
+	}
+	m.stop();
+	return (0);
 }
