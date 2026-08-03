58cb836b0a631823b3c78caef5be17fe69d9d44a
txavier <tchiadeestevaox@gmail.com>
2026-08-03 13:02:37 +0100

    Update search_musics function to accept SoundManager reference and iterate through music directories

--- srcs/search_ms.cpp
+++ srcs/search_ms.cpp
@@ -1,11 +1,14 @@
 # include "../includes/playIt.hpp"
 
-void	search_musics()
+void	search_musics(SoundManager& playlist)
 {
 	try{
-		for (auto entry : filesystem::directory_iterator("./")){
-			if (entry.is_directory())
-				std::cout << "found dir: " << entry.path() << std::endl;
+		for (auto entry : filesystem::directory_iterator("/home/tx/Música"))
+		{
+				std::cout << "found dir: " << entry.path().filename() << std::endl;
+
+				for (auto file : std::filesystem::directory_iterator(entry.path()))
+					playlist.addSong(file.path().filename());
 		}
 	}
 	catch (const filesystem::filesystem_error& error)
